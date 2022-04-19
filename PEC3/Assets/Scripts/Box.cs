c68f0403f29b80a0b2c37c993304801007fb1a49
Laia <laia.fustercorral@gmail.com>
2022-04-19 21:18:37 +0200

    Initial player movement functionality

--- PEC3/Assets/Scripts/Box.cs
+++ PEC3/Assets/Scripts/Box.cs
@@ -6,6 +6,16 @@ public class Box : MonoBehaviour
 {
     private void Start()
     {
+        CharacterMovement.OnMovingBox += MoveBox;
+    }
 
+    private void MoveBox(Vector2 direction)
+    {
+        transform.Translate(direction);
+    }
+
+    private void OnDestroy()
+    {
+        CharacterMovement.OnMovingBox -= MoveBox;
     }
 }
--- PEC3/Assets/Scripts/CharacterMovement.cs
+++ PEC3/Assets/Scripts/CharacterMovement.cs
@@ -1,6 +1,17 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour
 {
-    
+    public static Action<Vector2> OnMovingBox;
+
+    [SerializeField] private InputActionAsset playerInputAction;
+
+    private bool touchingBox;
+    private bool touchUp, touchDown, touchLeft, touchRight;
+
+    private void Start()
+    {
+    }
 }
