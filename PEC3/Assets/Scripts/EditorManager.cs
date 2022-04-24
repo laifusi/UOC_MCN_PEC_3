61521f0f398f536e022d18da8f619b049456cc3c
Laia <laia.fustercorral@gmail.com>
2022-04-24 14:55:44 +0200

    Grid creation, button selects image and scroll changes camera size

--- PEC3/Assets/Scripts/EditorManager.cs
+++ PEC3/Assets/Scripts/EditorManager.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EditorManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Sprite characterSprite;
+    [SerializeField] private Sprite starSprite;
+    [SerializeField] private Sprite boxSprite;
+    [SerializeField] private Sprite wallSprite;
+    [SerializeField] private GameObject backgroundPrefab;
+    [SerializeField] private TMP_InputField inputFieldX;
+    [SerializeField] private TMP_InputField inputFieldY;
+
+    private int[,] grid;
+
+    private void Start()
     {
-        
+        SetGrid();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetGrid()
     {
-        
+        DestroyBackground();
+        int x = int.Parse(inputFieldX.text);
+        int y = int.Parse(inputFieldY.text);
+        grid = new int[x, y];
+        for(int i = -x/2; i <= x/2; i++)
+        {
+            if (i == x / 2 && x % 2 == 0)
+                break;
+            for(int j = -y/2; j <= y/2; j++)
+            {
+                if (j == y / 2 && y % 2 == 0)
+                    break;
+
+                GameObject gridElement = Instantiate(backgroundPrefab, new Vector3(i, j, 0), Quaternion.identity, transform);
+
+                int posx = (int)gridElement.transform.position.x;
+                int posy = (int)gridElement.transform.position.y;
+                gridElement.transform.position = new Vector3(posx, posy, 0);
+            }
+        }
+    }
+
+    private void DestroyBackground()
+    {
+        foreach(Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    public void SelectElement(string elementChosen)
+    {
+        switch(elementChosen)
+        {
+            case "Character":
+                MouseControl.Instance.SetSprite(characterSprite);
+                break;
+            case "Star":
+                MouseControl.Instance.SetSprite(starSprite);
+                break;
+            case "Box":
+                MouseControl.Instance.SetSprite(boxSprite);
+                break;
+            case "Wall":
+                MouseControl.Instance.SetSprite(wallSprite);
+                break;
+        }
     }
 }
---
+++ PEC3/Assets/Scripts/MouseControl.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+public class MouseControl : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera; //Camera
+
+    private Vector3 mousePos; //Vector3 to save the position of the mouse
+    private float zOffset; //z offset to the camera
+    private SpriteRenderer spriteRenderer;
+
+    public static MouseControl Instance; //Instance of the MouseControl
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        zOffset = -mainCamera.transform.position.z;
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ChangePosition(CallbackContext context)
+    {
+        mousePos = context.ReadValue<Vector2>();
+        transform.position = GetMousePosition();
+        int posx = (int)transform.position.x;
+        int posy = (int)transform.position.y;
+        transform.position = new Vector3(posx, posy, transform.position.z);
+    }
+
+    public Vector3 GetMousePosition()
+    {        
+        mousePos.z += zOffset;
+        return mainCamera.ScreenToWorldPoint(mousePos);
+    }
+
+    public void SetSprite(Sprite sprite)
+    {
+        spriteRenderer.sprite = sprite;
+    }
+
+    public void Scroll(CallbackContext context)
+    {
+        var value = context.ReadValue<float>();
+        if (value > 0)
+            mainCamera.orthographicSize += 0.15f;
+        else
+            mainCamera.orthographicSize -= 0.15f;
+
+        if (mainCamera.orthographicSize < 2)
+            mainCamera.orthographicSize = 2;
+        else if (mainCamera.orthographicSize > 50)
+            mainCamera.orthographicSize = 50;
+    }
+}
